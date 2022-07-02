968cbaef6b2c5e7fff006a8f80daa8413f686209
Vikas <vk.pratap98@gmail.com>
2022-07-02 13:19:32 +0530

    mobile and desktop view completed

--- main.js
+++ main.js
@@ -55,9 +55,39 @@ const docFrag = document.createDocumentFragment();
 images.forEach((image)=>{
   const imageContainer = document.createElement("div");
   imageContainer.classList.add("w-full","relative");
-  imageContainer.innerHTML = `<img class="w-full" src=${image.imgSrc} alt=${image.imgTitle} />
+  imageContainer.innerHTML = `<img class="w-full gallery__image" src=${image.imgSrc} alt=${image.imgTitle} />
   <span class="absolute left-10 bottom-8 text-white/80" >${image.imgTitle}</span>`
   docFrag.append(imageContainer)
 })
 
 gallery.append(docFrag);
+
+const galleryImages = document.querySelectorAll(".gallery__image");
+const modal = document.querySelector(".image__modal");
+const modalContent = document.querySelector(".modal__content");
+// const modalImage = document.querySelector(".modal__Image");
+
+const handleOutsideClick = (e)=>{
+  const target = e.target;
+  console.log(modalContent.contains(target));
+  if(modalContent.contains(target)){
+    return
+  }
+  modal.classList.add("hidden");
+  modalContent.querySelector(".modal__image").remove()
+  modal.removeEventListener("mousedown ", handleOutsideClick)
+}
+
+galleryImages.forEach((image)=>{
+  image.addEventListener("click", ()=>{
+    modal.classList.remove("hidden");
+    const modalImage = document.createElement("img");
+    modalImage.src = image.src;
+    modalImage.classList.add("modal__image", "w-full");
+    modalContent.append(modalImage);
+    modalContent.classList.remove("scale-0")
+    modalContent.classList.add("scale-1")
+    // console.log(modalContent);
+    modal.addEventListener("click", handleOutsideClick)
+  })
+})
